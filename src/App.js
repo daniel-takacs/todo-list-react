3ae28dc32eb7b1173200007261dd879489d1146b
daniel-takacs <takacsd89@gmail.com>
2020-12-18 20:04:10 +0200

    handleTitleUpdate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import Form from './components/Form'
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Todo List</h1>
-      </header>
-      <Form />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    title: "text"
+  }
+
+  handleTitleUpdate = (newValue) =>{
+    this.setState({title: newValue})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>{this.state.title}</h1>
+        </header>
+        <Form handleTitleUpdate={this.handleTitleUpdate}/>
+      </div>
+    );
+  }
 }
 
 export default App;
