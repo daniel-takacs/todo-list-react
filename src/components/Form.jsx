0e00085a1dc11a55a8fff8d3334f2df8452b3c30
daniel-takacs <takacsd89@gmail.com>
2021-01-11 17:57:32 +0200

    code cleaning

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -13,10 +13,14 @@ export default function Form ({ inputText, setInputText, todos, setTodos }) {
     }
     return(
         <form className="form-style">
-            <input className="input-style"placeholder="What will you do today?"type="text"
-            onChange={inputHandler}
-            value={inputText}/>
-            <button className="add-button"onClick={submitHandler} type="submit">Add</button>
+            <input className="input-style"
+                   placeholder="What will you do today?"
+                   type="text"
+                   onChange={inputHandler}
+                   value={inputText}/>
+            <button className="add-button"
+                    onClick={submitHandler} 
+                    type="submit">Add</button>
         </form>
     );
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
 
-
 export default function Todo({ text, setTodos, todo, todos }) {
-
     const deleteHandler = ()=> {
         setTodos(todos.filter((el) => el.key !== todo.key))
     };
-
-  const completeHandler = ()=> {
-      setTodos(todos.map((item => {
+    const completeHandler = ()=> {
+        setTodos(todos.map((item => {
           if(item.key === todo.key) {
               return {
                   ...item, completed: !item.completed
               }
-          }
+            }
           return item;
-      })
-      )
+        })
+        )
       )}
     return (
         <div className="todo-list">
             <li className={`todo-item ${todo.completed ? "completed" : ""}`}>{text}</li>
-            
             <button onClick={completeHandler}><i class="fas fa-check-circle"></i></button>
             <button onClick={deleteHandler}><i class="fas fa-minus-circle"></i></button>
-           
         </div>
     )
 }
